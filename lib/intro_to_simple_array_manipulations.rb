1073df6f55647d5929b4c1051136d7da8b7b7b3e
darien bryant <darienbryant11@gmail.com>
2020-11-26 20:38:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,10 +15,10 @@ def pop_with_args(array)
   array.pop(2)
 end
 
-def using_shift(array, string)
-  
+def using_shift(array)
+  array.shift
 end
 
 def shift_with_args(array)
-  
+  array.shift(2)
 end
